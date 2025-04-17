690e7820a630cd3f06597fff9aa8255f786ec993
jackvloglive <jackvloglive@gmail.com>
2025-04-17 22:48:47 +1200

    added img/logo going to work on the logical side next taking a break

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { Clock } from "lucide-react";
 import { TimerDesign } from "./Components/Timer/Timer";
 import { TodoContainer } from "./Components/TodoItems/TodoBoxs";
 
 function App() {
   return (
     <>
-      <div>
-        <h1
-          style={{
-            display: "flex",
-            alignSelf: "center",
-            justifyContent: "center",
-            margin: "20px",
-          }}
-        >
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "center",
+          justifyContent: "center",
+          margin: "20px",
+        }}
+      >
+        <h1>
           Timer App
+          <Clock />
         </h1>
       </div>
       <TimerDesign />
